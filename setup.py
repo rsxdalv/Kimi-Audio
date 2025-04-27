8752d177b3909e1f726e2d24b5298a735dc58682
Roberts Slisans <roberts.slisans@gmail.com>
2025-04-27 00:16:04 +0000

    add missing __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kimia_infer",
-    version="0.1.0",
+    version="0.1.1",
     description="Kimi-Audio inference and toolkit package.",
     author="Moonshot AI",
     packages=find_packages(),
